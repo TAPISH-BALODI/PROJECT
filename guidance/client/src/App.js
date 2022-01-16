7019d09c5e36d564b4a0d32f7ba0a0a1d0c471dc
Tapish <tapishbalodi7@gmail.com>
2022-01-16 13:05:02 +0530

    installed typescript

--- guidance/client/src/App.js
+++ guidance/client/src/App.js
@@ -5,16 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <nav>
+      
           <h2 class="logo">Career Guidance </h2>
-               <div class="menu">
-                  <h3>ABOUT</h3>
-                  <h3>HELP</h3>
-                  <h3>LOGIN</h3>
-                  <h3>SIGN UP</h3>
-                </div>
+               <div class="navbar">
+                 
+                  <h3 class="menu">ABOUT</h3>
+                  <h3 class="menu">ABOUT</h3>
+                  <h3 class="menu">ABOUT</h3>
+                  <h3 class="menu">ABOUT</h3>
+               
+               </div>
+              
             
-            </nav>
             
     </div>
   );
