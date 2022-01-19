0bc8a6a1b134d4bc070bdc07a592061f3451ab47
Tapish <tapishbalodi7@gmail.com>
2022-01-19 15:15:29 +0530

    cssing the page

--- guidance/client/src/App.js
+++ guidance/client/src/App.js
@@ -5,16 +5,63 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
-          <h2 class="logo">Career Guidance </h2>
-               <div class="navbar">
-                 
-                  <h3 class="menu">ABOUT</h3>
-                  <h3 class="menu">ABOUT</h3>
-                  <h3 class="menu">ABOUT</h3>
-                  <h3 class="menu">ABOUT</h3>
+      <div >
+         <h2 class="logo">Career Guidance </h2>
+         <h4 class="motto">Road to your Dreams...</h4>   
+    </div>
+    <div class="navbar">
+     
+      <div class="menu">
+        <button class="menu-btn">Talks To Us</button>
+
+      </div> 
+      <div class="menu">
+        <button class="menu-btn">About Us</button>
+
+      </div>
+       <div class="menu">
+        <button class="menu-btn">How it works?</button>
+
+      </div>
+      <div className='menu'>
+
+        <input class="search-bar" placeholder="Type your Profession"type='text'></input>
+        
+      </div>
+      <div className='menu'>
+
+        <button class="menu-btn">SIGN UP</button>
+        <button  class="menu-btn">LOGIN</button>
+      </div>
                
-               </div>
+    </div>
+    <div class="body-image">
+     <div>
+              <div>
+                <h1 className='body-image-head-text'>Get expert career guidance and counselling from Our experts to discover which field you will enjoy and excel in.</h1>
+              </div>
+              <div className='body-image-card' >
+                <h2 class="body-image-card-text">8-9th Class Students</h2>
+                <button className='body-image-card-text-btn'>Look here</button>
+              </div>
+
+              <div className='body-image-card'>
+              <h2 class="body-image-card-text">10-12th Class Students</h2>
+                <button className='body-image-card-text-btn'>Decide here</button>
+              </div>
+
+              <div className='body-image-card'>
+              <h2 class="body-image-card-text">College Graduates </h2>
+                <button className='body-image-card-text-btn'>Choose here</button>
+              </div>
+
+              <div className='body-image-card'>
+              <h2 class="body-image-card-text">Working Professionals</h2>
+                <button className='body-image-card-text-btn'>Explore here</button>
+              </div>
+
+     </div>
+    </div>
               
             
             
