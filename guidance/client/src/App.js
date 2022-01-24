0900dff35dadf28d7548d42a86244e0ce9fcb365
Tapish <tapishbalodi7@gmail.com>
2022-01-24 16:09:28 +0530

    front page done

--- guidance/client/src/App.js
+++ guidance/client/src/App.js
@@ -12,10 +12,10 @@ function App() {
     <div class="navbar">
      
       <div class="menu">
-        <button class="menu-btn">Talks To Us</button>
         <button class="menu-btn">About Us</button>
+        <button class="menu-btn">Talks To Us</button>
         <button class="menu-btn">How it works?</button>
-        <input class="search-bar" placeholder="Type your Profession"type='text'></input> 
+       
         <button class="menu-btn-login-signup">SIGN UP</button>
         <button  class="menu-btn-login-signup">LOGIN</button>
       </div>     
@@ -23,36 +23,66 @@ function App() {
     </div>
     
     <div class="body-image">
-     <div>
-              <div>
+          <div className='cards'>
+              <div >
                 <h1 className='body-image-head-text'>Get expert career guidance and counselling from Our experts to discover which field you will enjoy and excel in.</h1>
               </div>
-              <div className='body-image-card' >
-                <h2 class="body-image-card-text">8-9th Class Students</h2>
-                <button className='body-image-card-text-btn'>Look here</button>
-              </div>
+                    <div className='body-image-card' >
+                        <h2 class="body-image-card-text">8-9th Class Students</h2>
+                        <h3 className='card-description'>know your ideal stream</h3>
+                          <div className='card-btn'>
+                              <button className='body-image-card-text-btn'>Look here</button>
+                          </div>
+                    </div>
 
-              <div className='body-image-card'>
-              <h2 class="body-image-card-text">10-12th Class Students</h2>
-                <button className='body-image-card-text-btn'>Decide here</button>
-              </div>
+                    <div className='body-image-card'>
+                        <h2 class="body-image-card-text">10-12th Class Students</h2>
+                        <h3 className='card-description'>know your ideal stream</h3>
+                          <div className='card-btn'>
+                            <button className='body-image-card-text-btn'>Decide here</button>
+                          </div>
+                     </div>
 
-              <div className='body-image-card'>
-              <h2 class="body-image-card-text">College Graduates </h2>
-                <button className='body-image-card-text-btn'>Choose here</button>
-              </div>
+                    <div className='body-image-card'>
+                        <h2 class="body-image-card-text">College Graduates</h2>
+                        <h3 className='card-description'>know your ideal stream</h3>
+                      <div className='card-btn'>
+                          <button className='body-image-card-text-btn'>Choose here</button>
+                      </div>
+                    </div>
 
-              <div className='body-image-card'>
-              <h2 class="body-image-card-text">Working Professionals</h2>
-                <button className='body-image-card-text-btn'>Explore here</button>
-              </div>
-
-     </div>
+                    <div className='body-image-card'>
+                        <h2 class="body-image-card-text">Working Professionals</h2>
+                        <h3 className='card-description'>know your ideal stream</h3>
+                      <div className='card-btn'>
+                          <button className='body-image-card-text-btn'>Explore here</button>
+                       </div>
+                    </div>
+            </div>
     </div>
-              
-            
-            
+
+    <div className='footer'>
+      <div className='footer-description'>
+        
+          <div className='footer-description'> 
+        <h3 className='footer-description-text' >Contact Us</h3>
+          </div>
+
+          <div className='footer-description'>
+         <h3 className='footer-description-text' >Suggestions</h3>
+          </div>
+
+        <div className='footer-description'>
+         <h3 className=' footer-description-text' >Connect with Us</h3> 
+        </div>
+  
+          
+
+      </div>
     </div>
+    </div>
+  
+    
   );
 }
 
--- guidance/server/index.js
+++ guidance/server/index.js
@@ -1,6 +1,22 @@
 const express= require('express');
 const app=express();
+const mysql=require('mysql');
 
+const db= mysql.createPool({
+    host:'localhost',
+    user:'root',
+    password:'password',
+    database:'studentRegistration',
+});
+
+
+
+app.get('/',(req,res)=>{
+
+    const sqlInsert='INSERT INTO studentRegistration {studentName,studentPassword,studentEmail'
+    db.query();
+    res.send("hello world");
+});
 app.listen(3001,()=>{
 console.log("running on port 3001");
 });
